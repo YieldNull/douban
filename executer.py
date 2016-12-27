8dc309907b889da0ef5ec0339a2dbcd7d9a7afda
YieldNull <yieldnull@gmail.com>
2016-12-27 22:47:36 +0800

    send mail

--- executer.py
+++ executer.py
@@ -2,7 +2,9 @@ import logging
 import signal
 import sys
 import socket
-
+import datetime
+import smtplib
+from email.mime.text import MIMEText
 from pydispatch import dispatcher
 from scrapy import signals
 from scrapy.settings.default_settings import LOG_FORMAT
@@ -11,6 +13,21 @@ from scrapy.utils import project, log
 from twisted.internet import reactor, defer
 from crawler.spiders.actor import ActorSpider
 from crawler.spiders.movie import MovieSpider, MovieLoginSpider
+
+
+def send_mail(reason):
+    body = "Scrapy has stopped at {:s}. Reason: {:s}.".format(
+        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        reason)
+
+    msg = MIMEText(body)
+    msg['Subject'] = "Scrapy Stopped"
+    msg['From'] = 'scrapy@scrapy.yieldnull.com'
+    msg['To'] = "yieldnull@gmail.com"
+
+    smtp = smtplib.SMTP('localhost')
+    smtp.send_message(msg)
+    smtp.quit()
 
 
 class Executor(object):
@@ -45,8 +62,11 @@ class Executor(object):
             if finished or self.interrupted:
                 break
 
-        self.logger.info('finished')
         reactor.stop()
+        self.logger.info('finished')
+
+        if not self.interrupted:
+            send_mail('finished')
 
     def execute(self):
         self._crawl()
@@ -60,6 +80,8 @@ class Executor(object):
 
             self.interrupted = True
             self.runner.stop()
+
+            send_mail(reason)
 
     def _handler_sys_signal(self, signum, frame):
         self.logger.info('KeyboardInterrupt. Stopping CrawlerRunner')
